58a24186dbff97af35f230793853228c824249b9
Chirayu Sharma <72456458+Chirayu123dot@users.noreply.github.com>
2021-05-12 13:08:02 +0530

    Updated to show images

--- ColorsActivity.java
+++ ColorsActivity.java
@@ -18,20 +18,20 @@ public class ColorsActivity extends AppCompatActivity {
         //String[] numbers = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
         ArrayList<Word> words = new ArrayList<>();
         // words.add("one")
-        words.add(new Word("red","weṭeṭṭi"));
-        words.add(new Word("green","chokokki"));
-        words.add(new Word("brown","ṭakaakki"));
-        words.add(new Word("gray","ṭopoppi"));
-        words.add(new Word("black","kululli"));
-        words.add(new Word("white","kelelli"));
-        words.add(new Word("dusty yellow","ṭopiisә"));
-        words.add(new Word("mustard yellow","chiwiiṭә"));
+        words.add(new Word("red","weṭeṭṭi", R.drawable.color_red));
+        words.add(new Word("green","chokokki", R.drawable.color_green));
+        words.add(new Word("brown","ṭakaakki", R.drawable.color_brown));
+        words.add(new Word("gray","ṭopoppi", R.drawable.color_gray));
+        words.add(new Word("black","kululli", R.drawable.color_black));
+        words.add(new Word("white","kelelli", R.drawable.color_white));
+        words.add(new Word("dusty yellow","ṭopiisә", R.drawable.color_dusty_yellow));
+        words.add(new Word("mustard yellow","chiwiiṭә", R.drawable.color_mustard_yellow));
 
 
         //  ArrayList<String> words1 = new ArrayList<>(); // creating an ArrayList
 
         //  ArrayAdapter<Word> itemsAdapter = new ArrayAdapter<>(this, R.layout.list_item, words);
-        WordAdapter adapter = new WordAdapter(this, words);
+        WordAdapter adapter = new WordAdapter(this, words, R.color.category_colors);
 
         ListView listView =  findViewById(R.id.list);
 
