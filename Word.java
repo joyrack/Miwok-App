050510e4b00f0245e6736d5e989c7fd88baf64a0
Chirayu Sharma <72456458+Chirayu123dot@users.noreply.github.com>
2021-05-12 12:50:46 +0530

    Added imageResourceId field

--- Word.java
+++ Word.java
@@ -1,16 +1,26 @@
 package com.example.miwok;
 
 public class Word {
-    private String engWord, miwokWord;
+    private final String engWord, miwokWord;
+    private final int imageResourceId;
 
     public Word(String eng, String miwok) {
         engWord = eng;
         miwokWord = miwok;
+        imageResourceId = -1;
+    }
+    public Word(String eng, String miwok, int imageId){
+        engWord = eng;
+        miwokWord = miwok;
+        imageResourceId = imageId;
     }
     public String getEngWord() {
         return engWord;
     }
     public String getMiwokWord() {
         return miwokWord;
+    }
+    public int getImageResourceId(){
+        return imageResourceId;
     }
 }
