6a1e2d2608a39e3d0a565467923e4464b935448e
Chirayu Sharma <72456458+Chirayu123dot@users.noreply.github.com>
2021-05-17 21:52:48 +0530

    Updated to play audio

--- NumbersActivity.java
+++ NumbersActivity.java
@@ -2,41 +2,57 @@ package com.example.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
+    private MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
-        // creating an array of english numbers (words)
-        //String[] numbers = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
-        ArrayList<Word> words = new ArrayList<>();
-       // words.add("one")
-        words.add(new Word("one","lutti", R.drawable.number_one));
-        words.add(new Word("two","otiiko", R.drawable.number_two));
-        words.add(new Word("three","tolookosu", R.drawable.number_three));
-        words.add(new Word("four","oyyisa", R.drawable.number_four));
-        words.add(new Word("five","massokka", R.drawable.number_five));
-        words.add(new Word("six","temmokka", R.drawable.number_six));
-        words.add(new Word("seven","kenekaku", R.drawable.number_seven));
-        words.add(new Word("eight","kawinta", R.drawable.number_eight));
-        words.add(new Word("nine","wo'e", R.drawable.number_nine));
-        words.add(new Word("ten","na'aacha", R.drawable.number_ten));
+        final ArrayList<Word> words = new ArrayList<>();
 
-      //  ArrayList<String> words1 = new ArrayList<>(); // creating an ArrayList
-       
-      //  ArrayAdapter<Word> itemsAdapter = new ArrayAdapter<>(this, R.layout.list_item, words);
+        words.add(new Word("one","lutti", R.drawable.number_one, R.raw.number_one));
+        words.add(new Word("two","otiiko", R.drawable.number_two, R.raw.number_two));
+        words.add(new Word("three","tolookosu", R.drawable.number_three, R.raw.number_three));
+        words.add(new Word("four","oyyisa", R.drawable.number_four, R.raw.number_four));
+        words.add(new Word("five","massokka", R.drawable.number_five, R.raw.number_five));
+        words.add(new Word("six","temmokka", R.drawable.number_six, R.raw.number_six));
+        words.add(new Word("seven","kenekaku", R.drawable.number_seven, R.raw.number_seven));
+        words.add(new Word("eight","kawinta", R.drawable.number_eight, R.raw.number_eight));
+        words.add(new Word("nine","wo'e", R.drawable.number_nine, R.raw.number_nine));
+        words.add(new Word("ten","na'aacha", R.drawable.number_ten, R.raw.number_ten));
+
         WordAdapter adapter = new WordAdapter(this, words, R.color.category_numbers);
 
         ListView listView =  findViewById(R.id.list);
 
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int audioId = words.get(position).getAudioResourceId();
+                 mediaPlayer = MediaPlayer.create(NumbersActivity.this, audioId);
+                 mediaPlayer.start();
+            }
+        });
 
     }
 }
