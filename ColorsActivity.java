436108322b7ec5ebe5b38ce5906142dc22772f63
Chirayu Sharma <72456458+Chirayu123dot@users.noreply.github.com>
2021-05-17 21:50:43 +0530

    Updated to play audio

--- ColorsActivity.java
+++ ColorsActivity.java
@@ -2,39 +2,48 @@ package com.example.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
+    private MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
-
-        // creating an array of english numbers (words)
-        //String[] numbers = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+        
         ArrayList<Word> words = new ArrayList<>();
-        // words.add("one")
-        words.add(new Word("red","weṭeṭṭi", R.drawable.color_red));
-        words.add(new Word("green","chokokki", R.drawable.color_green));
-        words.add(new Word("brown","ṭakaakki", R.drawable.color_brown));
-        words.add(new Word("gray","ṭopoppi", R.drawable.color_gray));
-        words.add(new Word("black","kululli", R.drawable.color_black));
-        words.add(new Word("white","kelelli", R.drawable.color_white));
-        words.add(new Word("dusty yellow","ṭopiisә", R.drawable.color_dusty_yellow));
-        words.add(new Word("mustard yellow","chiwiiṭә", R.drawable.color_mustard_yellow));
-
-
-        //  ArrayList<String> words1 = new ArrayList<>(); // creating an ArrayList
-
-        //  ArrayAdapter<Word> itemsAdapter = new ArrayAdapter<>(this, R.layout.list_item, words);
+        
+        words.add(new Word("red","weṭeṭṭi", R.drawable.color_red, R.raw.color_red));
+        words.add(new Word("green","chokokki", R.drawable.color_green, R.raw.color_green));
+        words.add(new Word("brown","ṭakaakki", R.drawable.color_brown, R.raw.color_brown));
+        words.add(new Word("gray","ṭopoppi", R.drawable.color_gray, R.raw.color_gray));
+        words.add(new Word("black","kululli", R.drawable.color_black, R.raw.color_black));
+        words.add(new Word("white","kelelli", R.drawable.color_white, R.raw.color_white));
+        words.add(new Word("dusty yellow","ṭopiisә", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
+        words.add(new Word("mustard yellow","chiwiiṭә", R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
+        
+        
         WordAdapter adapter = new WordAdapter(this, words, R.color.category_colors);
 
         ListView listView =  findViewById(R.id.list);
 
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int audioId = words.get(position).getAudioResourceId();
+                mediaPlayer = MediaPlayer.create(ColorsActivity.this, audioId);
+                mediaPlayer.start();
+            }
+        });
     }
 }
